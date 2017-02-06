b3fadc6e4b5638abb5a9162806acb162679812ef
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-06 10:52:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,11 +13,10 @@ $routeProvider
 // Home
 .when("/", {templateUrl: "partials/home.html", <span class="highlight">controller: "PageCtrl"</span>})
 // Pages
-.when("/about", {templateUrl: "partials/about.html", <span class="highlight">controller: "PageCtrl"</span>})
 .when("/collecties", {templateUrl: "partials/collecties.html", <span class="highlight">controller: "PageCtrl"</span>})
 .when("/contact", {templateUrl: "partials/contact.html", <span class="highlight">controller: "PageCtrl"</span>})
 // else 404
-.otherwise("/404", {templateUrl: "partials/404.html", <span class="highlight">controller: "PageCtrl"</span>});
+.otherwise("/404", {templateUrl: "404.html", <span class="highlight">controller: "PageCtrl"</span>});
 }]);
 
 /**
