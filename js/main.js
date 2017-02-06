5ffef1a0ecee98cf9eec23db5f02b506fbfa823f
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-06 15:59:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,5 +38,8 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   // Activates Tooltips for Social Links
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
-  })
+  });
+  $scope.slide = function (dir) {
+    $('#sieradenOverview').carousel(dir);
+};
 });
