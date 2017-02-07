a11030334a080e3b1e910e1b6d6a09985445da40
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 10:30:40 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,15 @@
 var items = [];
-var getter = $.getJSON( "partials/overview.json", function( data ) {
-      $.each( data, function( key, val ) {
-        items.push( "<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
-      });
-      items.push( "<div class='item active'>"+items.pop()+"</div>");
-      for ( var i = 0; i < items.length-1; i++ ) {
-        items[i] = "<div class='item'>"+items[i]+"</div>";
-      }
-});
+if(items.length != 0){
+    var getter = $.getJSON( "partials/overview.json", function( data ) {
+          $.each( data, function( key, val ) {
+            items.push( "<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
+          });
+          items.push( "<div class='item active'>"+items.pop()+"</div>");
+          for ( var i = 0; i < items.length-1; i++ ) {
+            items[i] = "<div class='item'>"+items[i]+"</div>";
+          }
+    });
+}
 $(document).ready(function(){
     getter.done(function(){
       $( "#sieradenItems" ).append( items.join("") );
