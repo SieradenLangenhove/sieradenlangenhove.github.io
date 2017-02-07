96937afb13c659b375cfea895acd47f12baca5c7
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 12:29:44 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,17 @@
-var items = [];
-var getter = $.getJSON( "partials/overview.json", function( data ) {
-      $.each( data, function( key, val ) {
-        items.push("<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
-        alert(key);
-      });
-      items.push( "<div class='item active'>"+items.pop()+"</div>");
-      for ( var i = 0; i < items.length-1; i++ ) {
-        items[i] = "<div class='item'>"+items[i]+"</div>";
-      }
-});
+
+var getter = 
 
 $(document).ready(function(){
-    getter.done(function(){
-      $( "#sieradenitems" ).append( items.join("") );
-    });
-    getter.fail(function(){
-        $( "#sieradenitems" ).append( "<p>Er is een probleem opgetreden.</p>" );
+    $.getJSON( "partials/overview.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push("<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
+            alert(key);
+        });
+        items.push( "<div class='item active'>"+items.pop()+"</div>");
+        for ( var i = 0; i < items.length-1; i++ ) {
+            items[i] = "<div class='item'>"+items[i]+"</div>";
+        }
+        $( "#sieradenitems" ).append( items.join("") );
     });
 });
