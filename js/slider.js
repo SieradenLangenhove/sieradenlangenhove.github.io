a0192bf7f709fb69e1611fb33c36fd334961cfd1
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 15:34:09 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.getJSON( "partials/overview.json", function( data ) {
         var items = [];
         $.each( data, function( key, val ) {
-            items.push("<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
+            items.push("<img src='img/"+key+"' alt='"+val+"' width='80%'><div class='carousel-caption'><p>"+val+"</p></div>");
         });
         $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
         $.each(items, function( index, value ) {
