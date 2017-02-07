c4d31c1e602e6869e85519c142e2a1f0c23707f2
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 12:38:01 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -4,10 +4,9 @@ $(document).ready(function(){
         $.each( data, function( key, val ) {
             items.push("<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
         });
-        items.push( "<div class='item active'>"+items.pop()+"</div>");
-        for ( var i = 0; i < items.length-1; i++ ) {
-            items[i] = "<div class='item'>"+items[i]+"</div>";
-        }
-        $( "#sieradenitems" ).append( items.join("") );
+        $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
+        $.each(items, function( index, value ) {
+            $( "#sieradenitems" ).append( "<div class='item'>"+value+"</div>" );
+        });
     });
 });
