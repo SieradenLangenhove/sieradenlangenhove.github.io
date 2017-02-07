4256293b82d402ca41b816f42920fa0903fec566
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 13:27:01 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    alert("whoop");
     $.getJSON( "partials/overview.json", function( data ) {
         var items = [];
         $.each( data, function( key, val ) {
