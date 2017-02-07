78bba71fd82217d8a9b2fde9aa38034e4c6b873f
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-07 10:41:28 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,20 @@
 var items = [];
 alert("hor");
-
-    alert("hi");
-    var getter = $.getJSON( "partials/overview.json", function( data ) {
-          $.each( data, function( key, val ) {
-            items.push( "<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
-          });
-          items.push( "<div class='item active'>"+items.pop()+"</div>");
-          for ( var i = 0; i < items.length-1; i++ ) {
-            items[i] = "<div class='item'>"+items[i]+"</div>";
-          }
-    });
+var getter = $.getJSON( "partials/overview.json", function( data ) {
+      $.each( data, function( key, val ) {
+        items.push( "<img src='img/"+key+"' alt='"+val+"' width='460'><div class='carousel-caption'><p>"+val+"</p></div>");
+      });
+      items.push( "<div class='item active'>"+items.pop()+"</div>");
+      for ( var i = 0; i < items.length-1; i++ ) {
+        items[i] = "<div class='item'>"+items[i]+"</div>";
+      }
+});
 
 $(document).ready(function(){
     getter.done(function(){
       $( "#sieradenItems" ).append( items.join("") );
+    });
+    getter.fail(function(){
+        $( "#sieradenItems" ).append( "<p>Er is een probleem opgetreden.</p>" );
     });
 });
