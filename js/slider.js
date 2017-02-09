2a3e4bb80b571c26f7767f7d25c7d99930fcb9ec
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-09 14:16:17 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
-    $.getJSON( "img/collectie.json", function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-            items.push("<img class='img-responsive center-block' src='img/collectie/"+key+"' alt='"+val+"'><div class='carousel-caption'><p>"+val+"</p></div>");
+    if($("#sieradenitems").length != 0){    
+        $.getJSON( "img/collectie.json", function( data ) {
+            var items = [];
+            $.each( data, function( key, val ) {
+                items.push("<img class='img-responsive center-block' src='img/collectie/"+key+"' alt='"+val+"'><div class='carousel-caption'><p>"+val+"</p></div>");
+            });
+            $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
+            $.each(items, function( index, value ) {
+                $( "#sieradenitems" ).append( "<div class='item'>"+value+"</div>" );
+            });
         });
-        $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
-        $.each(items, function( index, value ) {
-            $( "#sieradenitems" ).append( "<div class='item'>"+value+"</div>" );
+    }
+    else if($("#workshopitems").length !=0){
+        $.getJSON( "img/workshops.json", function( data ) {
+            var items = [];
+            $.each( data, function( key, val ) {
+                items.push("<img class='img-responsive center-block' src='img/workshops/"+key+"' alt='"+val+"'><div class='carousel-caption'><p>"+val+"</p></div>");
+            });
+            $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
+            $.each(items, function( index, value ) {
+                $( "#sieradenitems" ).append( "<div class='item'>"+value+"</div>" );
+            });
         });
-    });
+    }
 });
-alert("ho");
