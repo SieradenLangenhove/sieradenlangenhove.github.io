48b33e3667f5cbda9e25e21d27b6e980510f9b40
Arne Govaerts <argovaerts@users.noreply.github.com>
2017-02-09 14:24:15 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -17,9 +17,9 @@ $(document).ready(function(){
             $.each( data, function( key, val ) {
                 items.push("<img class='img-responsive center-block' src='img/workshops/"+key+"' alt='"+val+"'><div class='carousel-caption'><p>"+val+"</p></div>");
             });
-            $( "#sieradenitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
+            $( "#workshopitems" ).append( "<div class='item active'>"+items.pop()+"</div>" );
             $.each(items, function( index, value ) {
-                $( "#sieradenitems" ).append( "<div class='item'>"+value+"</div>" );
+                $( "#workshopitems" ).append( "<div class='item'>"+value+"</div>" );
             });
         });
     }
